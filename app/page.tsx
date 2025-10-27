799a1419e2ad32d1b37993fee353ad67e94f88f3
portoarco <arcoanggoro78@gmail.com>
2025-10-27 14:36:12 +0700

    Learn Button and Icon Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+"use client";
 import Image from "next/image";
 import { MuiTyphography } from "./components/MuiTypography";
+import { MuiButton } from "./components/MuiButton";
 
 export default function Home() {
   return (
     <>
-      <section>
-        <MuiTyphography />
+      <section className="flex justify-center">
+        {/* <MuiTyphography /> */}
+        <MuiButton />
       </section>
     </>
   );
